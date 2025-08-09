0b3d48c29a02585ae558537b7557351b916d5ee2
PaoloMar <PaoloMar_@hotmail.com>
2025-08-09 10:09:30 -0600

    Fechas completado y creación de comprensión de listas

--- PythonCourse/Intermediate/0_Dates.py
+++ PythonCourse/Intermediate/0_Dates.py
@@ -22,5 +22,49 @@ year_2026 = datetime(2026, 1, 1)
 
 print(year_2026)
 
-current_time = time()
+current_time = time(21, 6, 0)
+
+print(current_time.hour)
+print(current_time.minute)
+print(current_time.second)
+
+from datetime import date
+
+current_date = date.today()
+
+print(current_date.year)
+print(current_date.month)
+print(current_date.day)
+
+current_date = date(2025, 10, 6)
+
+print(current_date.year)
+print(current_date.month)
+print(current_date.day)
+
+current_date = date(current_date.year, current_date.month + 1, current_date.day)
+
+print(current_date.month)
+
+
+diff = year_2026 - now
+print(diff)
+
+diff = year_2026.date() - current_date
+print(diff)
+
+print(year_2026.time())
+
+from datetime import timedelta
+
+start_timedelta = timedelta(200, 10, 100, weeks=10)
+end_timedelta = timedelta(300, 100, 100, weeks=13)
+
+
+print(end_timedelta - start_timedelta)
+print(end_timedelta + start_timedelta)
+print(end_timedelta / start_timedelta)
+
+
+
 
